deb6d931ce4a979fed45e73bcfc1597f751a42ca
Macomi27 <macomfortitcham@gmail.com>
2025-04-24 19:59:33 +0100

    sorted issues with header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -19,11 +19,11 @@ const Header = () => {
     <nav className="header-nav">
         {/* <div className="navbar"> */}
             <div className="home-logo"><img src={logoLarge} alt="" /></div>
-            <ul className="nav-links">
+            <ul className="landing-headernavlinks">
                 <li className="home"><a href="#home">Home</a></li>
-                <li className="how-it-works"><a href="#howitworks">How It Works</a></li>
-                <li className="make-a-report"><a href="#makeareport">Make a Report</a></li>
-                <li className="contact"><a href="#contact">Contact Us</a></li>
+                <li className="how-it-works"><a href="#home">How It Works</a></li>
+                <li className="make-a-report"><a href="#home">Make a Report</a></li>
+                <li className="contact"><a href="#home">Contact Us</a></li>
             </ul>
             <div className="nav-btns">
                 <button className="btn-login" onClick={handleClickLogin}>Login</button>

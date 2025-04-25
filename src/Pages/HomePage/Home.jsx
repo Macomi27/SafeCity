561f7ecbf0477c67ee90668075aa529255c1d859
Macomi27 <macomfortitcham@gmail.com>
2025-04-25 09:02:46 +0100

    Made multiple adjustments on Home page

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "../../Components/Header/Header";
+import Header from '../../Components/Header/Header'
 import Footer from "../../Components/Footer/Footer"
 import AboutUs from './AboutUs'
 import CTA from './CTA'
